d780f8a50d421357108e040a939ab3dc9df8cd9b
Elena Lushnikova <elushnikova.256@gmail.com>
2020-07-31 14:07:28 +0500

    chore: Clear CLI-generated items

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <IndexPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import IndexPage from "./components/IndexPage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    IndexPage,
+  },
+};
 </script>
 
 <style>
